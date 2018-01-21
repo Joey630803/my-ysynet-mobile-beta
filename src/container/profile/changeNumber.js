f845237d3ad4343dc54d0b5f252fc44ba33ebac4
Joey <1297393811@qq.com>
2018-01-21 21:57:12 +0800

    success number

--- src/container/profile/changeNumber.js
+++ src/container/profile/changeNumber.js
@@ -25,13 +25,11 @@ class ChangeUserNumber extends Component {
 
   state = {
     hasError: false
-  }
+    }
 
   onSubmit = (e) => {
     e.stopPropagation();
-    this.props.form.validateFields((error, values) => {
-      if (!error) {
-        let val=values.newNumber.trim()
+        let val=this.refs.node.value
         const pattern = /^1[3|4|5|7|8]\d{1}\s\d{4}\s\d{4}$/;
         
           if(val.match(pattern)){
@@ -41,70 +39,81 @@ class ChangeUserNumber extends Component {
                   hasError: true,
                 });
                 Toast.info('手机格式不对!');
-              }
-      }
-    })
+            }
   }
 
   // const pattern = /^1[3|4|5|7|8]\d{1}\s\d{4}\s\d{4}$/;
   // pattern.test(str);
 
 
-
-  onErrorClick = () => {
-    if (this.state.hasError) {
-      Toast.info('手机格式不对!');
-    }
-  }
-  onFocus=(value)=>{
-    if (this.state.hasError) {
-      const {hasError}=this.state
-      this.setState({hasError:false})
-    }
-  }
-  onChange=(e)=>{
-    const E=e.target.value.trim()
-    const pattern = /\d|\s/g
-    if(!E.match(pattern)){
-    Toast.info('只能输入数字!')
-    }
-  }
 onKeyUp=(e)=>{
-  // const E=e.target.value.replace(/^ +| +$/g,'')
-  const E=e.target.value.trim()
-  let end = e.target.selectionEnd
-  let st = e.target.selectionStart
-  //const node = this.refs.nod
 
-  if(E!==''){
-    if(st===3||end===3){
-
-      e.target.value=E+" "
-
-      //e.target.setSelectionRange(3,3)
-
-    }else if(st===8||end===8){
-
-      e.target.value=E+" "
+  const mValue=e.target.value.replace(/\D/g,'');
   
-    }
-  }
-    else{
-      e.target.value=''
-  }
+  let inputValue
+    if (mValue !==''){
 
-  //删除设置
-  if (e.keyCode === 8) {
-    if(end===9){
-      e.target.selectionEnd=8
-      //e.target.setSelectionRange(8)
-    }
-    if(end===4){
-      e.target.selectionEnd=3
-    }
-    //e.target.value=e.target.value.trim()
+    let mLength = mValue.length;
+    if(mLength<=3){
+      inputValue=mValue
+    }else{
+      if (mLength <= 7) {
+        inputValue=mValue.substring(0, 3) + ' ' + mValue.substring(3, mLength)
+      } else {
+        inputValue=mValue.substring(0, 3) + ' ' + mValue.substring(3, 7) + ' ' + mValue.substring(7, 11)
+      }
   }
 }
+inputValue?this.refs.node.value=inputValue:this.refs.node.value=null
+}
+
+// onKeyUp=(e)=>{
+//   // if(e.target.value.match(/（[^\d]|[^\s]）/)){
+//   //   console.log('您输入的是非数字！')
+//   //  }
+// e.target.value=e.target.value.replace(/\D[^\s]/g,'')
+  
+//   const E=e.target.value.trim()
+//   let end = e.target.selectionEnd
+//   let st = e.target.selectionStart
+//   //const node = this.refs.nod
+
+//   if(E!==''){
+//     if(st===3||end===3){
+
+//       e.target.value=E+' '
+
+//       //e.target.setSelectionRange(3,3)
+
+//     }else if(st===8||end===8){
+
+//       e.target.value=E+' '
+  
+//     }
+//   }
+//     else{
+//       e.target.value=''
+//   }
+
+//   //删除设置
+//   if (e.keyCode === 8) {
+//     if(end===9){
+//       e.target.selectionEnd=8
+//       //e.target.setSelectionRange(8)
+//     }
+//     // if(st===9){
+//     //   e.target.selectionStart=8
+//     // }
+//     if(end===4){
+//       e.target.selectionEnd=3
+//       //e.target.selectionStart=3
+//     }
+//     // if(st===4){
+//     //   e.target.selectionStart=3
+//     // }
+//     //e.target.value=e.target.value.trim()
+//   }
+// }
   render () {
 
     const { getFieldProps } = this.props.form
@@ -120,24 +129,16 @@ onKeyUp=(e)=>{
         </NavBar>
 
         <WhiteSpace size='md' />
-
-          <InputItem
-          onFocus={this.onFocus}
-          error={this.state.hasError}
-          onErrorClick={this.onErrorClick}
+          {/* <InputItem
+            {...getFieldProps('phone')}
+            type="phone"
+            placeholder="186 1234 1234"
+          >手机号码</InputItem> */}
+          <input
           onKeyUp={this.onKeyUp}
-          onChange={this.onChange}
-          ref="nod"
+          ref="node"
           maxLength="13"
-              {...getFieldProps('newNumber', {
-                rules: [
-                  { required: true }
-                ]
-              })}
-              placeholder="修改手机号"
-              clear
-              moneyKeyboardAlign="left"
-              // onBlur={(v) => {console.log(v)}}
+          placeholder="修改手机号"
             />
         <WingBlank className="my_button">
           <Button type="primary" onClick={this.onSubmit}>保存</Button><WhiteSpace />
